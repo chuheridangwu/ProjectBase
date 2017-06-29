2be5c05db0087ff4eba5763cbc2e50661cb654d5
chuheridangwu <153100950@qq.com>
2017-06-29 10:04:46 +0800

    first commit

---
+++ ProjectBase/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ProjectBase
+//
+//  Created by dym on 2017/6/29.
+//  Copyright © 2017年 dym. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ProjectBase/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ProjectBase
+//
+//  Created by dym on 2017/6/29.
+//  Copyright © 2017年 dym. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
